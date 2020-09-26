46c4891dadea4ecfed163ac84fd928f4b944d3ef
briannzw <brianwijaya2003@gmail.com>
2020-09-27 03:38:20 +0700

    Add Project

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    public float maxSpeed = 15f;
+    [SerializeField]
+    private float actualSpeed;
+
+    private Vector3 movement;
+    private Animator anim;
+    private Rigidbody playerRigidbody;
+    private int floorMask;
+    private float camRayLength = 100f;
+
+    private void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+    //    float h = Input.GetAxis("Horizontal");
+    //    float v = Input.GetAxis("Vertical");
+
+    //    Move(h, v);
+        Turning();
+    //    Animating(h, v);
+    }
+
+    public void Move(float h, float v)
+    {
+        actualSpeed = Mathf.Clamp(speed, 0f, maxSpeed);
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * actualSpeed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    private void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if(Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    public void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
+}
---
+++ Assets/Scripts/PowerUps/SpawnPowerup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPowerup : MonoBehaviour
+{
+    #region Singleton
+    public static SpawnPowerup instance;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+    }
+    #endregion
+
+    public GameObject[] powerupPrefabs;
+    public GameObject player;
+
+    [HideInInspector]
+    public Collider playerCollider;
+    [HideInInspector]
+    public PlayerHealth playerHealth;
+    [HideInInspector]
+    public PlayerMovement playerMovement;
+
+    private void Start()
+    {
+        playerCollider = player.GetComponent<CapsuleCollider>();
+        playerHealth = player.GetComponent<PlayerHealth>();
+        playerMovement = player.GetComponent<PlayerMovement>();
+    }
+
+    public void Spawn(Vector3 position)
+    {
+        Instantiate(powerupPrefabs[Random.Range(0,powerupPrefabs.Length)], position, Quaternion.identity);
+    }
+}
